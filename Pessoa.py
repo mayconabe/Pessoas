3f96a4ca8eb949413678cd2ae2be30735edae49d
Maycon <maycon.abe01@gmail.com>
2020-03-09 20:37:20 -0300

    created get and set to nome

--- Pessoa.py
+++ Pessoa.py
@@ -3,3 +3,10 @@ class Pessoa:
   def __init__(self, nome, idade):
     self.nome = nome
     self.idade = idade
+
+  def get_nome(self):
+    return self.nome
+
+  def set_nome(self, nome):
+    self.nome = nome
+    return self
