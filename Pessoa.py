287d07e38d29b5ae12edec388f700232e31bb2ce
marleyabe <marleyabe@gmail.com>
2020-03-09 20:25:55 -0300

    idade

--- Pessoa.py
+++ Pessoa.py
@@ -1,5 +1,12 @@
 class Pessoa:
   
-  def __init__(self, nome, idade):
-    self.nome = nome
-    self.idade = idade
+	def __init__(self, nome, idade):
+		self.nome = nome
+		self.idade = idade
+
+	def set_idade(self, idade):
+		self.idade = idade
+		return self
+
+	def get_idade(self):
+		return self.idade
